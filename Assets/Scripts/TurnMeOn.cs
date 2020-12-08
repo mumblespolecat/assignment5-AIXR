7cc01e8b50438d2fcfe1148e604cee8f73d53d87
mumblespolecat <2031528@swamsea.ac.uk>
2020-12-08 16:58:54 +0000

    First Commit of the XR version - nothing works so far

---
+++ Assets/Scripts/TurnMeOn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class TurnMeOn : MonoBehaviour
+{
+    public GameObject dog;
+    
+    [YarnCommand("SwitchOn")]
+    public void SwitchOn()
+    {
+        dog.SetActive(true);
+    }
+}
